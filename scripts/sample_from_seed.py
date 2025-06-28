f7da4243001e9855ac132950fbd3b7a612cbc2bd
Sahil Jartare <tup19446@temple.edu>
2025-06-28 15:02:50 -0400

    3:02

--- scripts/sample_from_seed.py
+++ scripts/sample_from_seed.py
@@ -52,9 +52,8 @@ def main():
 
     print("Encoding seed image...")
     seed_tokens = get_seed_tokens(vqgan, SEED_IMAGE_PATH, SEED_TOKEN_COUNT)
-
     print("Generating sequence...")
-    sampled = sample_with_past(transformer, seed_tokens.unsqueeze(0), steps=256 - SEED_TOKEN_COUNT, temperature=1.0, top_k=100, top_p=0.95)
+    sampled = sample_with_past(seed_tokens.unsqueeze(0),transformer, steps=256 - SEED_TOKEN_COUNT, temperature=1.0, top_k=100, top_p=0.95)
 
     full_tokens = torch.cat([seed_tokens, sampled.squeeze(0)], dim=0)
 
