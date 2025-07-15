fc9cd32203f9f757ca155dec98ccb7cb633cfec1
Sahil Jartare <tup19446@temple.edu>
2025-07-15 11:05:20 -0400

    11:05

--- scripts/jigsaw_sample_with_image_token.py
+++ scripts/jigsaw_sample_with_image_token.py
@@ -13,7 +13,7 @@ import numpy as np
 CONFIG_PATH = "/root/logs/2021-04-03T19-39-50_cin_transformer/configs/2021-04-03T19-39-50-project.yaml"
 CHECKPOINT_PATH = "/root/logs/2021-04-03T19-39-50_cin_transformer/checkpoints/last.ckpt"
 SEED_IMAGE_PATH = "sample.png"
-CONDITIONAL_IMAGE_PATH = "sample2.png"
+CONDITIONAL_IMAGE_PATH = "sample.png"
 SEED_TOKEN_COUNT = 128# You can modify this value easily
 CONDITION_TOKEN_COUNT = 30
 MAX_LENGTH = 256
